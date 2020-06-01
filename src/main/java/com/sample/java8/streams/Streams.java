efa38e003888a5efc16769bed27701fd7bfab7b9
cjrequea <requenacj@gmail.com>
2020-06-01 13:39:03 +0200

    streams

---
+++ src/main/java/com/sample/java8/streams/Streams.java
@@ -0,0 +1,52 @@
+package com.sample.java8.streams;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+public class Streams {
+
+  public void filter(List<Integer> collection) {
+    collection.stream().filter(number -> isPrime(number)).forEach(System.out::println);
+  }
+
+  public void sort(List<Integer> collection, boolean asc) {
+    if(asc)
+      collection.stream().sorted().forEach(System.out::println);
+    else
+      collection.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
+  }
+
+  public void map(List<Integer> collection){
+    collection.stream().map(number -> number + 10).forEach(System.out::println);
+  }
+
+  public void limit(List<Integer> collection, int limit){
+    collection.stream().limit(limit).forEach(System.out::println);
+  }
+
+  public  void count(List<Integer> collection){
+    System.out.println(collection.stream().count());
+  }
+
+  /**
+   * Checks if a integer is a prime number
+   * @param number
+   * @return
+   */
+  private static boolean isPrime(int number) {
+    IntPredicate isDivisible = index -> number % index == 0;
+    return number > 1 && IntStream.range(2, number - 1).noneMatch(isDivisible);
+  }
+
+
+
+}
+
+
+  
+  
+  
---
+++ src/test/java/com/sample/java8/streams/StreamsTest.java
@@ -0,0 +1,68 @@
+package com.sample.java8.streams;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class StreamsTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  @Test
+  public void filter() {
+    Streams streams = new Streams();
+    List<Integer> integers = new ArrayList<>();
+    integers.add(0);
+    integers.add(1);
+    integers.add(2);
+    integers.add(3);
+    integers.add(4);
+    integers.add(5);
+    integers.add(6);
+    integers.add(7);
+    integers.add(8);
+    integers.add(9);
+
+    System.out.println("Print prime numbers");
+    System.out.println("===================");
+    streams.filter(integers);
+
+    System.out.println("");
+    System.out.println("Print numbers sorted desc");
+    System.out.println("===================");
+    streams.sort(integers, false);
+
+    System.out.println("");
+    System.out.println("Print numbers sorted asc");
+    System.out.println("===================");
+    streams.sort(integers, true);    System.out.println("");
+
+
+    System.out.println("");
+    System.out.println("Print numbers + 10");
+    System.out.println("===================");
+    streams.map(integers);
+
+    System.out.println("");
+    System.out.println("Print numbers from 0 to limit -1");
+    System.out.println("===================");
+    streams.limit(integers, 6);
+
+
+    System.out.println("");
+    System.out.println("Print the count of numbers");
+    System.out.println("===================");
+    streams.count(integers);
+  }
+}
