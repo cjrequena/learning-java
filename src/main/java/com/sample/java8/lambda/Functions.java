a5789ed1815e2c5b1cdcc9c490f47a7af712dd64
cjrequea <requenacj@gmail.com>
2020-10-27 16:44:49 +0100

    misc·

--- src/main/java/com/sample/java8/lambda/Functions.java
+++ src/main/java/com/sample/java8/lambda/Functions.java
@@ -1,5 +1,7 @@
 package com.sample.java8.lambda;
 
+import java.util.Stack;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -12,12 +14,31 @@ public class Functions {
   /**
    *
    */
-  public  static Function<String, Integer> toInteger = Integer::valueOf;
+  public static Function<String, Integer> toInteger = Integer::valueOf;
 
   /**
    *
    */
-  public  static Function<String, String> backToString = toInteger.andThen(String::valueOf);
+  public static Function<String, String> backToString = toInteger.andThen(String::valueOf);
+
+  /**
+   *
+   */
+  public static BiFunction<Double, Double, Double> add = (operandA, operandB) -> operandA + operandB;
+
+  /**
+   *
+   */
+  public static BiFunction<Double, Double, Double> multiply = (operandA, operandB) -> operandA * operandB;
+
+  /**
+   *
+   * @param operands
+   * @param operation
+   */
+  protected static Double applyOperationToOperands(Stack<Double> operands, BiFunction<Double, Double, Double> operation) {
+    return operation.apply(operands.pop(), operands.pop());
+  }
 
   /**
    *
@@ -27,7 +48,7 @@ public class Functions {
     StringBuffer searchStringBuffer = new StringBuffer(search.length());
     Matcher matcher = bound.matcher(search);
     while (matcher.find()) {
-      matcher.appendReplacement(searchStringBuffer, matcher.group().toUpperCase().replace("_",""));
+      matcher.appendReplacement(searchStringBuffer, matcher.group().toUpperCase().replace("_", ""));
     }
     return matcher.appendTail(searchStringBuffer).toString();
   };
--- src/test/java/com/sample/java8/lambda/FunctionsTest.java
+++ src/test/java/com/sample/java8/lambda/FunctionsTest.java
@@ -2,6 +2,8 @@ package com.sample.java8.lambda;
 
 import org.junit.Test;
 
+import java.util.Stack;
+
 import static org.junit.Assert.assertEquals;
 
 public class FunctionsTest {
@@ -14,6 +16,28 @@ public class FunctionsTest {
   @Test
   public void convertToCamelCaseTest() {
     assertEquals(Functions.convertToCamelCase.apply("hello_world"), "helloWorld");
+  }
+
+  @Test
+  public void addTest() {
+    assertEquals(Functions.add.apply(Double.valueOf(2), Double.valueOf(2)), Double.valueOf(4));
+  }
+
+  @Test
+  public void multiplyTest() {
+    assertEquals(Functions.multiply.apply(Double.valueOf(3), Double.valueOf(3)), Double.valueOf(9));
+  }
+
+  @Test
+  public void applyOperationToOperandsTest() {
+    Stack<Double> stack = new Stack<>();
+    stack.push(Double.valueOf(3));
+    stack.push(Double.valueOf(3));
+    assertEquals(Functions.applyOperationToOperands(stack, Functions.add),  Double.valueOf(6));
+    stack.push(Double.valueOf(3));
+    stack.push(Double.valueOf(3));
+    assertEquals(Functions.applyOperationToOperands(stack, Functions.multiply),  Double.valueOf(9));
+
   }
 
 }
