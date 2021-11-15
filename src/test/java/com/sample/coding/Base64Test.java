201101f9024aafd0d17965dcd23f41952668b33a
cjrequena <requenacj@gmail.com>
2021-11-15 13:35:54 +0100

    Base64 example

---
+++ src/test/java/com/sample/coding/Base64Test.java
@@ -0,0 +1,28 @@
+package com.sample.coding;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Base64Test {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void encode() {
+    assertEquals(Base64.encode("Hello::World"),  "SGVsbG86Oldvcmxk");
+  }
+
+  @Test
+  void decode() {
+    assertEquals(Base64.decode("SGVsbG86Oldvcmxk"),  "Hello::World");
+  }
+}
